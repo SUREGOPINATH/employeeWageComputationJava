a64b6d4960f6bd1b5b03db20403e543365aed1d7
SURE GOPINATH <suregopinath@gmail.com>
2020-10-17 11:54:46 +0530

    employee Attendance with parttime daily wage Check using switch case for month ie 20 days or 100 working hours is done

--- src/com/programs/employeeWageComputationJava.java
+++ src/com/programs/employeeWageComputationJava.java
@@ -1,5 +1,15 @@
 package com.programs;
 import com.utility.Utilities;
+
+/**
+ * @author : Sure Gopinath
+ * Created Date : 17th Oct 2020
+ * Functionality : This is employee wage computation program
+ *                 contains employee attendance will taken using
+ *                 random function through that calculating the
+ *                 employee wages for month
+ *
+ * */
 public class employeeWageComputationJava {
 
     public static void main (String[] args)
--- src/com/utility/Utilities.java
+++ src/com/utility/Utilities.java
@@ -1,7 +1,13 @@
 package com.utility;
 
 import java.util.Arrays;
-
+/**
+ * @author : Sure Gopinath
+ * Created Date : 17th Oct 2020
+ * Functionality : This is utilities program contains all
+ *                 logic and code of program is present inside
+ *                 this file
+ * */
 public class Utilities {
 
 
@@ -10,6 +16,12 @@ public class Utilities {
     static  int wages = 0;
     static int totalWages = 0;
     static int[] arrayOfWages = new int[20];
+
+    /**
+     * Functionality : This is only function that called in employee
+     *                 wage computation program
+     *
+     * */
     public void employeeDailyWagePerMonth () {
 
         int index = 0;
@@ -26,10 +38,23 @@ public class Utilities {
         System.out.println("all wages in a month are "+ Arrays.toString(arrayOfWages));
         System.out.println("total wages of employee in a month is "+totalWages+"\ntotal working Days are "+totalDays+"\ntotal working hours are "+totalHrs);
     }
+
+    /**
+     * @return -> this return the value obtain from employeeAttendance method
+     *
+     * */
     public static int attendanceChecker () {
 
         return employeeAttendance((int) Math.floor(Math.random() *10 ) % 3);
     }
+
+    /**
+     * @param attendance -> it has value from attendanceChecker(0,1,2)
+     *                      0 -> employee Absent
+     *                      1 -> employee Fulltime
+     *                      2 -> employee parttime
+     * @return -> return the value wage from employeeDailyWage method
+     * */
     public static int employeeAttendance (int attendance) {
 
         switch (attendance) {
@@ -50,6 +75,14 @@ public class Utilities {
         System.out.println("employee daily wage is " + wages+"\n");
         return wages;
     }
+
+    /**
+     * @param hrs -> it has value from employeeAttendance(0,4,8)
+     *               0 -> working hours is 0 i.e employee absent
+     *               4 -> working hours is 4 i.e employee parttime
+     *               8 -> working hours is 8 i.e employee Fulltime
+     *
+     * */
     public static int employeeDailyWage (int hrs) {
         return hrs*wagePerHr;
     }
