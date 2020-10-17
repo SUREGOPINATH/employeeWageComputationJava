de681af6d805694c7af76f81c0cf6fe39223dc66
SURE GOPINATH <suregopinath@gmail.com>
2020-10-17 10:31:32 +0530

    employee Attendance with daily wage Check is done

--- src/com/utility/Utilities.java
+++ src/com/utility/Utilities.java
@@ -3,16 +3,24 @@ package com.utility;
 public class Utilities {
 
     static int isFulltime = 1;
+    static int wagePerHr = 20;
     public void attendanceChecker () {
 
         employeeAttendance((int) Math.floor(Math.random() *10 ) % 2);
     }
     public static void employeeAttendance (int attendance) {
 
-        if (attendance == isFulltime)
+        if (attendance == isFulltime) {
             System.out.println("employee present");
-        else
+            System.out.println("employee daily wage is " + employeeDailyWage(8));
+        }
+        else {
             System.out.println("employee Absent");
+            System.out.println("employee daily wage is " + employeeDailyWage(0));
+        }
+    }
+    public static int employeeDailyWage (int hrs) {
+        return hrs*wagePerHr;
     }
 
 }
