2bb81798a06d300a9e03f12192a5273ca7189043
SURE GOPINATH <suregopinath@gmail.com>
2020-10-17 10:36:26 +0530

    employee Attendance with parttime daily wage Check is done

--- src/com/utility/Utilities.java
+++ src/com/utility/Utilities.java
@@ -3,16 +3,21 @@ package com.utility;
 public class Utilities {
 
     static int isFulltime = 1;
+    static int isParttime = 2;
     static int wagePerHr = 20;
     public void attendanceChecker () {
 
-        employeeAttendance((int) Math.floor(Math.random() *10 ) % 2);
+        employeeAttendance((int) Math.floor(Math.random() *10 ) % 3);
     }
     public static void employeeAttendance (int attendance) {
 
         if (attendance == isFulltime) {
-            System.out.println("employee present");
+            System.out.println("employee fulltime present");
             System.out.println("employee daily wage is " + employeeDailyWage(8));
+        }
+        else if (attendance == isParttime) {
+            System.out.println("employee parttime Present");
+            System.out.println("employee daily wage is "+ employeeDailyWage(4));
         }
         else {
             System.out.println("employee Absent");
